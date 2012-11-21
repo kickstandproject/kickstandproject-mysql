d09e523f50a6cf11b0963197a584ed71c54b5e99
Paul Belanger <paul.belanger@polybeacon.com>
2012-11-21 11:50:54 -0500

    Typo
    Signed-off-by: Paul Belanger <paul.belanger@polybeacon.com>

--- manifests/function/database.pp
+++ manifests/function/database.pp
@@ -3,6 +3,7 @@ define database(
   exec { "create-${name}-db":
     command => "mysql -uroot -e \"create database ${name};\"",
     require => Class['mysql::server::service'],
+  }
 }
 
 # vim:sw=2:ts=2:expandtab:textwidth=79
