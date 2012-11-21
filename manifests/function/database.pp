beec07e385aa2f22e373d2aa6fec61e2fca61cb3
Paul Belanger <paul.belanger@polybeacon.com>
2012-11-21 12:05:41 -0500

    Simplify command
    Signed-off-by: Paul Belanger <paul.belanger@polybeacon.com>

--- manifests/function/database.pp
+++ manifests/function/database.pp
@@ -1,10 +1,9 @@
 define mysql::function::database(
 ) {
-  exec { "create-${name}-db":
+  exec { "mysql --defaults-file=/root/.my.cnf -uroot -e \"create database ${name};\"":
     creates => "/var/lib/mysql/${name}",
-    command => "mysql --defaults-file=/root/.my.cnf -uroot -e \"create database ${name};\"",
     require => Class['mysql::server::service'],
   }
 }
 
-# vim:sw=2:ts=2:expandtab:textwidth=79
+# vim:sw=2:ts=2:expandtab
