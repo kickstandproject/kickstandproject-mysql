30baaace4be9ea5c55aca2d7de6d16cb80d9e240
Paul Belanger <paul.belanger@polybeacon.com>
2012-11-21 13:41:06 -0500

    Typo
    Signed-off-by: Paul Belanger <paul.belanger@polybeacon.com>

--- manifests/function/user.pp
+++ manifests/function/user.pp
@@ -2,7 +2,7 @@ define mysql::function::user(
   $password,
   $host = 'localhost'
 ) {
-  $sql = "mysql --defaults-file=/root/.my.cnf -uroot -e \"CREATE USER '${name}'@'${host}' IDENTIFIED BY '${password}';\"":
+  $sql = "mysql --defaults-file=/root/.my.cnf -uroot -e \"CREATE USER '${name}'@'${host}' IDENTIFIED BY '${password}';\""
 
   exec { $sql:
     require => Class['mysql::server::service'],
